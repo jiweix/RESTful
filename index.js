a034a65eed78de26ab7bc98d76d85a439fae591f
Jiwei Xu <jx734@nyu.edu>
2017-03-10 10:46:09 -0500

    Created a running app

---
+++ index.js
@@ -0,0 +1,17 @@
+var express     =   require("express");
+var app         =   express();
+var bodyParser  =   require("body-parser");
+var router      =   express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended" : false}));
+
+router.get("/",function(req,res){
+    res.json({"error" : false,
+              "message" : "Hello World"});
+});
+
+app.use('/',router);
+
+app.listen(8080);
+console.log("Listening to PORT 8080");
