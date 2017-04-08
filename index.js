39a16dd393cbeb4b47348292fd1a14c8745c11d4
Jiwei Xu <jx734@nyu.edu>
2017-04-08 14:41:45 -0400

    add post tests

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
-var express     =   require("express");
-var app         =   express();
-var bodyParser  =   require("body-parser");
-var router      =   express.Router();
-var application =   require('./applications.js');
-var DB = require('./db');
+var express           =   require("express");
+var app               =   express();
+var bodyParser        =   require("body-parser");
+var router            =   express.Router();
+var application       =   require('./applications.js');
+var expressValidator  =   require('express-validator');
+var DB                =   require('./db');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
+app.use(expressValidator());
+// validation function
+function validateApplication(req, res, next) {
+  req.checkBody('semester', 'invalid semester').notEmpty();
+  req.checkBody('school', 'invalid school').notEmpty();
+  req.checkBody('result', 'invlid result').notEmpty();
+  req.checkBody('year', 'invalid year').notEmpty().isInt();
+  req.checkBody('gpa', 'invalid gpa').notEmpty();
+  req.checkBody('gre', 'invalid gre').notEmpty().isInt();
+  req.checkBody('toefl', 'invalid toefl').notEmpty().isInt();
+  var errors = req.validationErrors();
+  if (errors) {
+    var response = { errors: [] };
+    errors.forEach(function(err) {
+      response.errors.push(err.msg);
+    });
+    res.statusCode = 400;
+    return res.json(response);
+  }
+  return next();
+}
 
 app.get("/",function(req,res){
     res.json({"API name" : "application info",
               "url" : "/applications"});
 });
-app.get('/applications', application.findAll);
-app.get('/applications/:id', application.findById);
-app.post('/applications', application.addApplication);
-app.put('/applications/:id', application.updateApplication);
-app.delete('/applications/:id', application.deleteApplication);
+router.get('/', application.findAll);
+router.get('/:id', application.findById);
+router.post('/', validateApplication, application.addApplication);
+router.put('/:id', validateApplication, application.updateApplication);
+router.delete('/:id', application.deleteApplication);
 
-app.use('/',router);
+app.use('/applications',router);
 
 var env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
--- test.js
+++ test.js
@@ -9,20 +9,30 @@ chai.use(chaiHttp);
 
 const TEST_APP_1 = {
   "semester": "summer",
-  "year": "2012",
-  "gpa": "3.5",
-  "gre": "320",
-  "toefl": "101",
+  "year": 2012,
+  "gpa": 3.5,
+  "gre": 320,
+  "toefl": 101,
   "school": "NYU",
   "result": "yes"
 };
 
 const TEST_APP_2 = {
   "semester": "spring",
-  "year": "2015",
-  "gpa": "4.0",
-  "gre": "340",
-  "toefl": "120",
+  "year": 2015,
+  "gpa": 4.0,
+  "gre": 340,
+  "toefl": 120,
+  "school": "Yale",
+  "result": "yes"
+}
+
+const TEST_APP_INVALID = {
+  "semester": "spring",
+  "year": "2015y",
+  "gpa": 4.0,
+  "gre": 340,
+  "toefl": 120,
   "school": "Yale",
   "result": "yes"
 }
@@ -64,35 +74,11 @@ describe('CS Applications API Tests', function() {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
-          res.body.should.have.property('year').eql("2012");
+          res.body.should.have.property('year').eql(2012);
           res.body.should.have.property('school').eql("NYU");
           res.body.should.have.property('result').eql("yes");
-          res.body.should.have.property('gre').eql("320");
+          res.body.should.have.property('gre').eql(320);
           done();
-    });
-  });
-
-  it('it should POST one application, and GET it back by _id', (done) => {
-    var id;
-    chai.request('http://localhost:8080')
-        .post('/applications')
-        .send(TEST_APP_1)
-        .end((err, res) => {
-          id = res.body._id;
-          res.should.have.status(201);
-          res.body.should.be.a('object');
-          res.body.should.have.property('year').eql("2012");
-          chai.request('http://localhost:8080')
-              .get('/applications/' + id)
-              .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.property('year').eql("2012");
-                res.body.should.have.property('school').eql("NYU");
-                res.body.should.have.property('result').eql("yes");
-                res.body.should.have.property('gre').eql("320");
-                done();
-          });
     });
   });
 
@@ -114,19 +100,19 @@ describe('CS Applications API Tests', function() {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
-          res.body.should.have.property('year').eql("2015");
+          res.body.should.have.property('year').eql(2015);
           res.body.should.have.property('school').eql("Yale");
           res.body.should.have.property('result').eql("yes");
-          res.body.should.have.property('gre').eql("340");
+          res.body.should.have.property('gre').eql(340);
           chai.request('http://localhost:8080')
               .get('/applications/' + _id)
               .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('object');
-                res.body.should.have.property('year').eql("2015");
+                res.body.should.have.property('year').eql(2015);
                 res.body.should.have.property('school').eql("Yale");
                 res.body.should.have.property('result').eql("yes");
-                res.body.should.have.property('gre').eql("340");
+                res.body.should.have.property('gre').eql(340);
                 done();
           });
     });
@@ -134,13 +120,59 @@ describe('CS Applications API Tests', function() {
 
   it('it should PUT one application with invalid id and get 404 error', (done) => {
     chai.request('http://localhost:8080')
-        .put('/applications' + "12345566666")
+        .put('/applications/' + "12345566666")
         .send(TEST_APP_2)
         .end((err, res) => {
           res.should.have.status(404);
           res.body.should.be.a('object');
           //console.log(err);
           //console.log(res.body);
+          done();
+    });
+  });
+
+  it('it should PUT one invalid application format and get 400 error', (done) => {
+    chai.request('http://localhost:8080')
+        .put('/applications/' + _id)
+        .send(TEST_APP_INVALID)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a('object');
+          done();
+    });
+  });
+
+  it('it should POST one application, and GET it back by _id', (done) => {
+    var id;
+    chai.request('http://localhost:8080')
+        .post('/applications')
+        .send(TEST_APP_1)
+        .end((err, res) => {
+          id = res.body._id;
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.body.should.have.property('year').eql(2012);
+          chai.request('http://localhost:8080')
+              .get('/applications/' + id)
+              .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('year').eql(2012);
+                res.body.should.have.property('school').eql("NYU");
+                res.body.should.have.property('result').eql("yes");
+                res.body.should.have.property('gre').eql(320);
+                done();
+          });
+    });
+  });  
+
+  it('it should POST one invalid application format and get 400 error', (done) => {
+    chai.request('http://localhost:8080')
+        .post('/applications')
+        .send(TEST_APP_INVALID)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a('object');
           done();
     });
   });
